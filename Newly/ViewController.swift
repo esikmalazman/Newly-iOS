26fac11660e94286a8c8f7c430a0ae98d6911365
Ikmal Azman <opikmalazman@gmail.com>
2021-10-22 18:51:22 +0800

    Initial Commit

---
+++ Newly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Newly
+//
+//  Created by Ikmal Azman on 22/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
