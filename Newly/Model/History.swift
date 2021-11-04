ae89e3e318aa9c9639303b35f289ee4617c17fd4
Ikmal Azman <opikmalazman@gmail.com>
2021-11-05 07:43:08 +0800

    create data model for history

---
+++ Newly/Model/History.swift
@@ -0,0 +1,13 @@
+//
+//  Text.swift
+//  Newly
+//
+//  Created by Ikmal Azman on 04/11/2021.
+//
+
+import Foundation
+
+struct History {
+    let category : String
+    let fullText : String
+}
--- Newly/Presenter/IdentifyNewsPresenter.swift
+++ Newly/Presenter/IdentifyNewsPresenter.swift
@@ -7,15 +7,31 @@
 
 import Foundation
 
-protocol IdentifyNewsPresenterDelegate : AnyObject {}
+protocol IdentifyNewsPresenterDelegate : AnyObject {
+    func presentPredictions(_ text : String)
+    func presentText(results : String)
+}
 
 final class IdentifyNewsPresenter {
     
     private weak var delegate : IdentifyNewsPresenterDelegate?
-    private var sourceType : String?
+    private let textAnalystInteractor =  TextAnalysisInteractor()
     
     func setViewDelegate(delegate : IdentifyNewsPresenterDelegate) {
         self.delegate = delegate
+    }
+    
+    func makePredictions(text : String) {
+        if let results = textAnalystInteractor.predictText(text) {
+         //   print("Results : \(results.capitalized)")
+            delegate?.presentPredictions(results.capitalized)
+        }
+    }
+    
+    func analyseTextIn(image : Data) {
+        textAnalystInteractor.processImageToText(imageData: image) { results in
+            self.delegate?.presentText(results : results)
+        }
     }
     
 }
